915b00b3cf973ceb800c90afeb465a753091941b
RadhikaRJ <radhika7002@gmail.com>
2021-05-27 18:46:59 +0530

    h10 error resultion

--- index.js
+++ index.js
@@ -41,10 +41,8 @@ app.use(function (err, req, res, next) {
   });
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
+let port = process.env.PORT || 3000;
+
 app.listen(port);
 
 // () => {
