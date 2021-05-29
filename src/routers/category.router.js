d846b3b7d54116fb8900af9037f3bdaa8940939f
RadhikaRJ <radhika7002@gmail.com>
2021-05-29 13:34:56 +0530

    catgeory get-post id & middleware

--- src/routers/category.router.js
+++ src/routers/category.router.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const router = express.Router();
-
+const { extend } = require("lodash");
 const { Category } = require("../models/category.model");
+
+function categoryMiddleware(req, res, next) {
+  if (req.params) {
+    console.log("req params: ", req.params);
+    console.log("req type: ", req.method);
+  }
+  next();
+}
+
+router.use("/:id", categoryMiddleware);
 
 router
   .route("/")
@@ -11,13 +21,11 @@ router
       res.json({ success: true, category });
     } catch (error) {
       console.log("error while fetching category data");
-      res
-        .status(500)
-        .json({
-          success: false,
-          errMsg: "error while fetching category data",
-          err: error.message,
-        });
+      res.status(500).json({
+        success: false,
+        errMsg: "error while fetching category data",
+        err: error.message,
+      });
     }
   })
   .post(async (req, res) => {
@@ -28,13 +36,60 @@ router
       res.json({ success: true, newCategory });
     } catch (error) {
       console.log("error while inserting category data");
+      res.status(500).json({
+        success: false,
+        errMsg: "error while inserting category data",
+        err: error.message,
+      });
+    }
+  });
+
+router.param("categoryId", async (req, res, next, categoryId) => {
+  try {
+    const category = await Category.findById(categoryId);
+    if (!category) {
       res
-        .status(500)
-        .json({
-          success: false,
-          errMsg: "error while inserting category data",
-          err: error.message,
-        });
+        .status(400)
+        .json({ success: false, message: "Unable to fetch category" });
+    }
+    req.category = category;
+    next();
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Unaable to fetch category",
+      errMsg: error.message,
+    });
+  }
+});
+
+router
+  .route("/:categoryId")
+  .get((req, res) => {
+    try {
+      let { category } = req;
+      res.json({ success: true, category });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: "Unable to find catgeory",
+        errMsg: error.message,
+      });
+    }
+  })
+  .post(async (req, res) => {
+    let { category } = req;
+    const updatedCategory = req.body;
+    try {
+      category = extend(category, updatedCategory);
+      category = await category.save();
+      res.json({ success: true, category });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: "Unable to update category",
+        errMsg: error.message,
+      });
     }
   });
 
