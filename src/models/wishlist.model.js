cd7269159ee80b6bea998659a10690968304adb0
RadhikaRJ <radhika7002@gmail.com>
2021-05-29 18:34:30 +0530

    update wishlist model

--- src/models/wishlist.model.js
+++ src/models/wishlist.model.js
@@ -2,9 +2,18 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
+const wishlistSubschema = new Schema(
+  {
+    product_id: String,
+  },
+  {
+    _id: false,
+  }
+);
+
 const WishlistSchema = new Schema(
   {
-    wishlist_product_list: { type: Schema.Types.Mixed },
+    wishlist_product_list: [wishlistSubschema],
   },
   {
     timestamps: true,
