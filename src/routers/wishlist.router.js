1fa33257bf47b6c3f19f42aa1b463d733563c383
RadhikaRJ <radhika7002@gmail.com>
2021-05-29 17:50:07 +0530

    update wishlist

--- src/routers/wishlist.router.js
+++ src/routers/wishlist.router.js
@@ -1,8 +1,18 @@
 const express = require("express");
-
+const { extend } = require("lodash");
 const router = express.Router();
 
-const { WishList } = require("../models/wishlist");
+const { WishList } = require("../models/wishlist.model");
+
+function wishlistMiddleware(req, res, next) {
+  if (req.params) {
+    console.log("Req params: ", req.params);
+    console.log("Req type: ", req.method);
+  }
+  next();
+}
+
+router.use("/:id", wishlistMiddleware);
 
 router
   .route("/")
@@ -29,6 +39,54 @@ router
         success: false,
         message: "unable to insert wishlist data",
         errMsg: error.message,
+      });
+    }
+  });
+
+router.param("wishlistId", async (req, res, next, wishlistId) => {
+  try {
+    const wishlist = await WishList.findById(wishlistId);
+    if (!wishlist) {
+      res.json({ success: false, message: "Unable to find wishlist" });
+    }
+    req.wishlist = wishlist;
+    next();
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      errMsg: error.message,
+      message: "Unable to find wishlist",
+    });
+  }
+});
+
+router
+  .route("/:wishlistId")
+  .get((req, res) => {
+    try {
+      let { wishlist } = req;
+      res.json({ success: true, wishlist });
+    } catch (error) {
+      res.status(400).json({
+        success: false,
+        errMsg: error.message,
+        message: "Unable to fetch wishlist",
+      });
+    }
+  })
+  .post(async (req, res) => {
+    let { wishlist } = req;
+    const updatedWishlist = req.body;
+
+    try {
+      wishlist = extend(wishlist, updatedWishlist);
+      wishlist = await wishlist.save();
+      res.json({ success: true, wishlist });
+    } catch (error) {
+      res.status(400).json({
+        success: false,
+        errMsg: error.message,
+        message: "Unable to update wishlist",
       });
     }
   });
